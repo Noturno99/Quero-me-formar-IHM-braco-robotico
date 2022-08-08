f2ff766ac9b99013a4eb548790cf111e04c4bcfd
Gabriel Corrêa <70723135+Noturno99@users.noreply.github.com>
2022-08-07 23:04:31 -0300

    Update Server-Toradex.cpp

--- IHM/Server-Toradex.cpp
+++ IHM/Server-Toradex.cpp
@@ -11,25 +11,23 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-    int port = 5560;
-    // Buffer para receber e enviar mensagens
-    char msg[1500];
+    int port = 5560; // Porta que o usuário deve digitar para realizar a conexão
+    char msg[1500]; // Buffer para receber e enviar mensagens
      
     // Criação do socket server
     sockaddr_in servAddr;
-    bzero((char*)&servAddr, sizeof(servAddr));
-    servAddr.sin_family = AF_INET;
-    servAddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    servAddr.sin_port = htons(port);
- 
-    int serverSd = socket(AF_INET, SOCK_STREAM, 0);
+    bzero((char*)&servAddr, sizeof(servAddr)); // Zera a região de memória
+    servAddr.sin_family = AF_INET; // AF_INET = Adress family IPv4, que designa o tipo de endereço com o qual o socket poderá se comunicar
+    servAddr.sin_addr.s_addr = htonl(INADDR_ANY); // Converte a ordem de byte do host para a ordem de byte do network, com comprimento do dado sendo long
+    servAddr.sin_port = htons(port); // Converte a ordem de byte do host para a ordem de byte do network, com comprimento do dado sendo short
+    int serverSd = socket(AF_INET, SOCK_STREAM, 0); // SOCK_STREAM = tipo de protocolo TCP/IP
     if(serverSd < 0)
     {
         printf("\n Erro em inicializar o socket server. \n");
         exit(0);
     }
 
-    int bindStatus = bind(serverSd, (struct sockaddr*) &servAddr, sizeof(servAddr));
+    int bindStatus = bind(serverSd, (struct sockaddr*) &servAddr, sizeof(servAddr)); // Associa a porta (local adress) com o socket server
     if(bindStatus < 0)
     {
         printf("\n Erro no building do socket no endereço local. \n");
@@ -37,12 +35,12 @@ int main(int argc, char *argv[])
     }
 
     printf("\n Esperando o client para conectar. \n");
-    listen(serverSd, 5);
+    listen(serverSd, 5); // Anuncia que o server está disposto a aceitar conexões
 
+    // Criação de um socket para receber as mensagens do client
     sockaddr_in newSockAddr;
     socklen_t newSockAddrSize = sizeof(newSockAddr);
-
-    int newSd = accept(serverSd, (sockaddr *)&newSockAddr, &newSockAddrSize);
+    int newSd = accept(serverSd, (sockaddr *)&newSockAddr, &newSockAddrSize); // Estabelece uma conexão de entrada
     if(newSd < 0)
     {
         printf("\n Erro no client aceitar o convite. \n" );
@@ -57,18 +55,18 @@ int main(int argc, char *argv[])
     while(1)
     {
         printf("\n Esperando mensagem do client... \n");
-        memset(&msg, 0, sizeof(msg));
+        memset(&msg, 0, sizeof(msg)); // Lê a mensagem do client
         bytesLeitura += recv(newSd, (char*)&msg, sizeof(msg), 0);
-        if(!strcmp(msg, "exit"))
+        if(!strcmp(msg, "exit")) // Confere se a comunicação foi encerrada pelo client
         {
             printf("\n Client saiu da sessão...\n" );
             break;
         }
-        printf("\n Posição, velocidade, torque: %s \n", msg);
+        printf("\n Posição, velocidade, torque: %s \n", msg); // Printa os dados lidos
         bytesEscrita += send(newSd, (char*)&msg, strlen(msg), 0);
     }
 
-    // Fechar sockets
+    // Encerra sockets
     close(newSd);
     close(serverSd);
     printf("\n Conexão interrompida... \n");
